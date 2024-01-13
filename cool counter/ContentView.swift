cda3a81e49380104ca16f9840c754aeef9b772f4
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2024-01-13 12:18:47 -0500

    Initial Commit

---
+++ cool counter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cool counter
+//
+//  Created by Richard Nkanga on 13/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cool counter/cool_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  cool_counterApp.swift
+//  cool counter
+//
+//  Created by Richard Nkanga on 13/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct cool_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
